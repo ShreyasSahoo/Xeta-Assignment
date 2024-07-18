658262adf19dac20b024d4fc5442be7c489797b2
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-07-18 18:24:37 +0530

    Initial Commit

---
+++ Xeta-Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xeta-Assignment
+//
+//  Created by Shreyas Sahoo on 18/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xeta-Assignment/Xeta_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xeta_AssignmentApp.swift
+//  Xeta-Assignment
+//
+//  Created by Shreyas Sahoo on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Xeta_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
